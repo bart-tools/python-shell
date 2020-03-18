27adae2a6c16f127dfc1512d123787b01d1be213
Alex Sokolov <volokos.alex@gmail.com>
2020-03-18 20:59:44 +0200

    PS-33: Bump version to 1.0.3

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='python-shell',
-    version='1.0.2',
+    version='1.0.3',
     description='Python Shell Wrapper library',
     long_description=long_description,
     long_description_content_type='text/markdown',
